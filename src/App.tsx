178b9308897ef8aa165c1c88e900f2308f0a70aa
violi <violinkk29@gmail.com>
2023-11-11 20:22:05 +0300

    set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import './App.css'
-
 function App() {
-  return (
-    <div>
-
-    </div>
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0B0D17",
+        secondary: "#D0D6F9",
+        primary_text: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
